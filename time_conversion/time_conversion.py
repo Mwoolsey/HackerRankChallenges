bc823d0a715f0363a28249ac3638d324c76a209e
mwoolsey <mwoolsey625@gmail.com>
2017-07-19 23:49:55 -0700

    finished time_conversion

---
+++ time_conversion/time_conversion.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import sys
+
+def timeConversion(s):
+    if not len(s) == 10:
+        return "Bad input"
+    hour = int(s[:2])
+    minutes = int(s[3:5])
+    seconds = int(s[6:8])
+    period = s[8:10]
+    if (1 <= hour <= 12) and (0 <= minutes <= 59) and (0 <= seconds <= 59):
+        if period == "AM":
+            if hour == 12:
+                return "00:" + str(minutes) + ":" + str(seconds)
+            else:
+                return str(hours).zfill(2) + ":" + str(minutes).zfill(2) + ":" + str(seconds).zfill(2)
+        elif period == "PM":
+            if hour == 12:
+                return str(hour).zfill(2) + ":" + str(minutes).zfill(2) + ":" + str(seconds).zfill(2)
+            else:
+                return str(12 + hour).zfill(2) + ":" + str(minutes).zfill(2) + ":" +  str(seconds).zfill(2)
+        else:
+            return "Bad input"
+    else:
+        return "Bad input"
+
+
+s = input().strip()
+result = timeConversion(s)
+print(result)
